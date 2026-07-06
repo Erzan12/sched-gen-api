cb197fdb31bb49cfba625b2180d885d2e410090a
erzan12 <do.earljan@gmail.com>
2026-07-06 16:11:51 +0800

    config: enabled cors for main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  
+  // await app.listen(process.env.PORT ?? 3001);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // your Next.js frontend
+    methods: 'GET,POST',
+    credentials: true,
+  });
+
+  await app.listen(3001, () => {
+    console.log('Server is running at http://localhost:3001');
+  })
+  // console.log("PORT RUNNING:", port)
 }
 bootstrap();
