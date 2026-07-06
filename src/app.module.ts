13f3216aa3119f92fd05a21a127910a0c00d3375
erzan12 <do.earljan@gmail.com>
2026-07-06 16:11:26 +0800

    config: added proper imports, controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LlmService } from './modules/llm/llm.service';
+import { AvailabilityService } from './modules/meetings/availability/availability.service';
+import { MeetingsService } from './modules/meetings/meetings/meetings.service';
+import { ChatController } from './modules/chat/chat.controller';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './config/prisma/prisma/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  controllers: [AppController, ChatController],
+  providers: [AppService, MeetingsService, LlmService, AvailabilityService, PrismaService],
 })
 export class AppModule {}
